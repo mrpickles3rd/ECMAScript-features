74b67f6256213ceb5f56a9c3746b89d5ba9e6a92
Dan B <mrpickles3rd@googlemail.com>
2024-10-30 15:29:02 +0000

    "Fix" lintting issues for es20218

--- es2018/3.promise-finally.js
+++ es2018/3.promise-finally.js
@@ -1,8 +1,9 @@
 let isLoading = true;
 fetch('http://somesite.com/users')
-    .then(data => data.json())
-    .catch(err => console.error(err))
-    .finally(() => {
-        isLoading = false;
-        console.log('Finished loading!!');
-    })
+  .then((data) => data.json())
+  .catch((err) => console.error(err))
+  .finally(() => {
+    console.log('Finished loading!!', isLoading);
+    isLoading = false;
+    console.log('Finished loading!!', isLoading);
+  });
