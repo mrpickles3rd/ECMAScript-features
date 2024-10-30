b2ce1d6757021182d4148892e380f2c82a26a6d1
Dan B <mrpickles3rd@googlemail.com>
2024-10-30 15:15:56 +0000

    "Fix" lint issues for es2016

--- es2016/array-includes.js
+++ es2016/array-includes.js
@@ -1,8 +1,9 @@
-const array = [1,2,3,4,5,6];
-if(array.includes(5)){
-    console.log("Found an element");
+const array = [1, 2, 3, 4, 5, 6];
+if (array.includes(5)) {
+  console.log('Found an element');
 }
 
-let numbers = [1, 2, 3, 4, NaN, ,];
+// eslint-disable-next-line no-sparse-arrays
+const numbers = [1, 2, 3, 4, NaN, ,];
 console.log(numbers.includes(NaN)); // true
 console.log(numbers.includes(undefined)); // true
--- es2016/exponential-operator.js
+++ es2016/exponential-operator.js
@@ -1,7 +1,8 @@
-//Prior ES7
-const cube = x => Math.pow(x, 3);
+// Prior ES7
+// eslint-disable-next-line prefer-exponentiation-operator, no-restricted-properties
+const cube = (x) => Math.pow(x, 3); // TODO: `no-restricted-properties` ???
 console.log(cube(3)); // 27
 
-//Using ES7
-const cube1 = x => x ** 3;
+// Using ES7
+const cube1 = (x) => x ** 3;
 console.log(cube1(3)); // 27
